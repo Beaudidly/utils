6f5e6b7d179b2e32911c3dbebc15d1a2203dcf19
Austin Bodzas <austinbodzas@gmail.com>
2016-07-30 01:01:48 -0400

    finishing functionality touches on dot updater

--- updatedot.sh
+++ updatedot.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+# Austin Bodzas - July 2016
+
+# Little bash utility I wrote a little too late at night
+# These are my early days of bash scripting, I feel like
+# I will get addicted to this...
+
+
+# TODO write help flag functionality to describe how to use it 
+# and what it does
+
 FILELIST=()
+DOTDIR="$HOME/pkgs/dotfiles/"
+FNAMEREGEX='(\w*\.?\w+)$'
+RED='\033[0;31m'
+GREEN='\033[0,32m'
 
 track ()
 {
-    for arg in "$@"
-    do
-        echo "12312"
-    done
-
     # check if the file exists
     if [[ ! -f "$1" ]];
     then 
@@ -35,15 +44,54 @@ track ()
 update () {
     for file in "${FILELIST[@]}"
     do
-        cp "$file" "$HOME/pkgs/dotfiles"
+        cp "$file" "$DOTDIR"
     done
 }
 
+isoutdated () {
+    if [[ ! -f "$1" ]]
+    then
+        ( >&2 echo "Error: non-file passed to isoutdated()" ) 
+        exit 1
+    fi
+
+    homefile="$1"
+    repofile=""
+
+    if [[ $homefile =~ $FNAMEREGEX ]]
+    then
+        repofile="$DOTDIR${BASH_REMATCH[1]}"
+    else
+        ( >&2 echo "Error: couldn't parse filename in isoutdated()")
+        exit 1
+    fi
+    
+    homedate=$(stat -c %Y "$homefile")
+    repodate=$(stat -c %Y "$repofile")
+
+    if [[ $homedate -lt $repodate ]] 
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
 list () {
-    echo "Files being tracked:"
+        
+    printf "Files being tracked:\n\n"
     for file in "${FILELIST[@]}"
     do
-        echo "$file"
+        if [[ $file =~ $FNAMEREGEX ]]
+        then
+            output="${BASH_REMATCH[0]}"
+            if isoutdated $file;
+            then
+                printf "\t${RED}${output}\n"
+            else
+                printf "\t${GREEN}${output}\n"
+            fi
+        fi
     done
 }
 
